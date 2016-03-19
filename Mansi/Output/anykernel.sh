9dac207b3b87d2e532755ee0a4ecba0f5be204c7
Mansi <slfl@mail.ru>
2016-03-19 14:36:56 +0300

    build: new build script + tools

---
+++ Mansi/Output/anykernel.sh
@@ -0,0 +1,70 @@
+# AnyKernel 2.0 Ramdisk Mod Script 
+
+kernel.string=Mansi
+do.cleanup=1
+
+# shell variables
+block=/dev/bootimg;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# set up extracted files and directories
+ramdisk=/tmp/anykernel/ramdisk;
+bin=/tmp/anykernel/tools;
+split_img=/tmp/anykernel/split_img;
+patch=/tmp/anykernel/patch;
+bindir=/system/bin;
+
+chmod -R 755 $bin;
+mkdir -p $ramdisk $split_img;
+cd $ramdisk;
+
+OUTFD=`ps | grep -v "grep" | grep -oE "update(.*)" | cut -d" " -f3`;
+ui_print() { echo "ui_print $1" >&$OUTFD; echo "ui_print" >&$OUTFD; }
+
+# dump boot and extract ramdisk
+dump_boot() {
+  dd if=$block of=/tmp/anykernel/boot.img;
+  $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Dumping/unpacking image failed. Aborting...";
+    echo 1 > /tmp/anykernel/exitcode; exit;
+  fi;
+  gunzip -c $split_img/boot.img-ramdisk.gz | cpio -i;
+}
+
+# repack ramdisk then build and write image
+write_boot() {
+  cd $split_img;
+  cmdline=`cat *-cmdline`;
+  board=`cat *-board`;
+  base=`cat *-base`;
+  pagesize=`cat *-pagesize`;
+  kerneloff=`cat *-kerneloff`;
+  ramdiskoff=`cat *-ramdiskoff`;
+  tagsoff=`cat *-tagsoff`;
+  if [ -f *-second ]; then
+    second=`ls *-second`;
+    second="--second $split_img/$second";
+    secondoff=`cat *-secondoff`;
+    secondoff="--second_offset $secondoff";
+  fi;
+  if [ -f /tmp/anykernel/zImage ]; then
+    kernel=/tmp/anykernel/zImage;
+  else
+    kernel=`ls *-zImage`;
+    kernel=$split_img/$kernel;
+  fi;
+  cd $ramdisk;
+  find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+  $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff --output /tmp/anykernel/boot-new.img;
+  if [ $? != 0 -o `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+    ui_print " "; ui_print "Repacking image failed. Aborting...";
+    echo 1 > /tmp/anykernel/exitcode; exit;
+  fi;
+  dd if=/tmp/anykernel/boot-new.img of=$block;
+}
+
+#  run scripts
+dump_boot;
+write_boot;
--- build.sh
+++ build.sh
@@ -1,17 +1,88 @@
-# Info for Java
-export JAVA_HOME=/usr/lib/jvm/java-6-oracle
+#!/bin/bash
 
-# Set patch for ToolChain
-#export CROSS_COMPILE=~/toolchains/arm-linux-androideabi-4.9.4/bin/arm-cortex_a7-linux-gnueabihf-
-export CROSS_COMPILE=~/toolchains/arm-linux-androideabi-4.7.7/bin/arm-linux-androideabi-
+###############################################################################
+# To all DEV around the world via Mansi :)                                    #
+#                                                                             #
+# 1.) use the "bash"                                                          #
+# chsh -s /bin/bash `whoami`                                                  #
+#                                                                             #
+# 2.) delete old files                                                        #
+# rm -r out                                                                   #
+#                                                                             #
+# 3.) now you can build my kernel                                             #
+# ./build.sh                                                                  #
+#                                                                             #
+# Have fun and update me if something nice can be added to my source.         #
+###############################################################################
 
-# Workaround for + appended on kernelrelease, may not be required
-#export LOCALVERSION=
-export ARCH=arm
+OUT_DIR="out"
+KERNEL_DIR=$PWD
+TYPE=huawei89_we_kk
+KERN_IMG=${OUT_DIR}/target/product/${TYPE}/obj/KERNEL_OBJ/arch/arm/boot/zImage
+BUILD_START=$(date +"%s")
 
-# This is essential to build a working kernel!
+#Set Color
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+###############################################################################
+# Set configs
+###############################################################################
+
+# Name a kernel
 export KBUILD_BUILD_USER="Mansi"
 export KBUILD_BUILD_HOST="MSI"
 
-# Build command
-./makeMtk -t -o=TARGET_BUILD_VARIANT=user huawei89_we_kk r k
+# Arch
+export ARCH=arm
+export SUBARCH=arm
+
+# gcc 4.7.7 
+export CROSS_COMPILE=~/toolchains/arm-linux-androideabi-4.7.7/bin/arm-linux-androideabi-
+#export CROSS_COMPILE=~/toolchains/arm-linux-androideabi-4.9.4/bin/arm-cortex_a7-linux-gnueabihf-
+
+# Other
+export JAVA_HOME=/usr/lib/jvm/java-6-oracle
+export USE_CCACHE=1
+
+mkdir ${OUT_DIR}
+
+
+###############################################################################
+# Build system
+###############################################################################
+echo -e "$cyan Clean old files $nocol";
+	rm ${OUT_DIR}/target/product/${TYPE}/obj/KERNEL_OBJ/arch/arm/boot/Image
+	rm ${KERN_IMG}
+	rm ${KERNEL_DIR}/Mansi/Output/zImage
+
+echo -e "$cyan Build kernel $nocol";
+	./makeMtk -t -o=TARGET_BUILD_VARIANT=user ${TYPE} n k
+
+if ! [ -a $KERN_IMG ]; then
+echo -e "$red Kernel Compilation failed! Fix the errors! $nocol";
+	exit 1
+fi
+
+
+###############################################################################
+# Other configs, after build
+###############################################################################
+
+echo -e "$cyan Copy kernel $nocol";
+	cp ${KERN_IMG}  ${KERNEL_DIR}/Mansi/Output/zImage
+	cd ${KERNEL_DIR}/Mansi/Output/
+
+echo -e "$cyan Build flash file $nocol";
+	zipfile="Mansi_M40_($(date +"%d-%m-%Y(%H.%M%p)")).zip"
+	zip -r ../${zipfile} ramdisk anykernel.sh zImage patch tools META-INF -x *kernel/.gitignore*
+
+	BUILD_END=$(date +"%s")
+	DIFF=$(($BUILD_END - $BUILD_START))
+
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol";
+
+echo "Enjoy Mansi kernel";
